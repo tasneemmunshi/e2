add455e00f235f0aa829abdbe4e55403e3450de6
Tasneem Munshi <tasneem.munshi@gmail.com>
2019-09-14 23:26:04 +0000

    Initialized Project 1 Application

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: Tasneem Munshi
++ Production URL: <http://p1.tasneemmunshi.xyz>
+
+## Outside resources
+*your list of outside resources go here*
+
+## Notes for instructor
+n/a
---
+++ p1/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang='en'>
+<head>
+	<title>Project 1 Application</title>
+	<meta charset='utf-8'>
+</head>
+<body>
+	<h1>Tasneem Munshi</h1>
+	<p>Hello! My name is Tasneem Munshi, but most call me Tas. I work for a pharmaceutical company based in NYC - I am a business analyst for the Business Technology department. I am taking this course because I am interested in learning a new skill-set which will further advance me as a technologist. I hope that the course will provide with teachings that I can apply while on the job. In my free time, I am a freelance makeup artist and enjoying spending time with family and friends. </p>
+</body>
+</html>
